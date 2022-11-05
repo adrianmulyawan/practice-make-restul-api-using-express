018d333bdc62ed2b952c9eab9173205a4e6c91ce
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-11-05 19:53:16 +0700

    edit verify token, with checking the token is blacklist or not

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const db = require('../models');
+
+const Blacklist = db.Blacklist;
 
 const verifyToken = async (req, res, next) => {
   // Cek Jika Header Terdapat Param Authorization Standar Bearer
   if (req.headers.authorization) {
     // Mengambil Bearer Token dari Header
     const token = req.headers.authorization.split(' ')[1];
+
+    // Pengecekan token
+    // Apakah token yang dikirim masuk kedalam daftar Blacklist / Tidak 
+    const checkBlackList = await Blacklist.findOne({
+      where: {
+        token: token
+      }
+    });
+    // Jika blacklist reutrn ini 
+    if (checkBlackList) {
+      return res.status(401).send({
+        auth: false,
+        message: 'Your token is blacklist, please login again'
+      });
+    }
 
     // Proses Verifikasi Token Dengan Secret Key yang telah dibuat
     const secret = 'secret';
