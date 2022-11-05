c00d788049a60f87b1d623dfa6298530bc71c6bf
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-11-05 15:17:42 +0700

    implements middleware in employe routes

--- routes/employe.js
+++ routes/employe.js
@@ -6,13 +6,15 @@ const {
   updateEmploye, 
   deleteEmploye 
 } = require('../controllers/employe.controller');
+const { verifyToken } = require('../middleware/verify');
 
 const router = express.Router();
 
-router.get('/employe', getEmployes);
-router.post('/employe', addEmploye);
-router.get('/employe/:id', getDetailEmploye);
-router.put('/employe/:id', updateEmploye);
-router.delete('/employe/:id', deleteEmploye);
+// Tambahkan Middleware Verify Pada Route Employe
+router.get('/employe', verifyToken, getEmployes);
+router.post('/employe', verifyToken, addEmploye);
+router.get('/employe/:id', verifyToken, getDetailEmploye);
+router.put('/employe/:id', verifyToken, updateEmploye);
+router.delete('/employe/:id', verifyToken, deleteEmploye);
 
 module.exports = router;
